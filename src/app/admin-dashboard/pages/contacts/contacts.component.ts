e37fe00477585d456ba20f7b905e0e618f786273
Mauro Miskinich <miskinichmauro@gmail.com>
2025-10-04 01:12:37 -0300

    Delete

--- src/app/admin-dashboard/pages/contacts/contacts.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-contacts',
-  imports: [],
-  templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.css',
-})
-export class ContactsComponent { }
